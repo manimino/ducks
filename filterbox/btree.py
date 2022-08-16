30b3382cc9e1b6f4f7a3b36b0e2c06f614d07578
Theo Walker <theo.ca.walker@gmail.com>
2022-08-16 11:49:56 -0400

    cleanup

--- filterbox/btree.py
+++ filterbox/btree.py
@@ -119,12 +119,7 @@ class BTree:
             # OOBTree oddity: it allows a non-comparable object on the first insert, but
             # if it gets one, all future inserts will fail.
             # So let's raise a TypeError if the very first insert is a non-comparable type.
-            try:
-                key > key
-            except TypeError:
-                raise TypeError(
-                    f"Cannot add object with attribute {key}: Type must be comparable. Implement __lt__()."
-                )
+            key > key
         if key not in self.tree:
             self.length += 1
         self.tree[key] = value
