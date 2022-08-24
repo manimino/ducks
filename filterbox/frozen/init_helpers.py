bc04a2f86ba3a30c085a9e57943f8a4c3c8c0d8a
Theo Walker <theo.ca.walker@gmail.com>
2022-08-24 07:27:30 -0400

    docs fixes

--- filterbox/frozen/init_helpers.py
+++ filterbox/frozen/init_helpers.py
@@ -1,26 +1,6 @@
-"""
-When FilterBox is initialized with objects, it would be slow to add the objects one at a time. Instead, we can
-analyze the list of objects, plan out which containers will be created, and put the objects in those containers.
-
-These functions are also used during FrozenFilterBox creation, although the final data structures there are different.
-
-Workflow:
- - Hash the given attribute for all objs
- - Sort the hashes
- - Group within each hash by matching value
- - Get counts of each unique hash (via run-length encoding)
- - Return all information that init needs to create containers for the objects
-
-Note that we cannot simply sort by value; the values may not be comparable. Hence we hash, then group by value.
-
-The total time is very close to simply making a dict-of-set for all objects; it's about the best you can do in Python.
-Running the workflow takes between 600ms (low-cardinality case) and 1.5s (high-cardinality case) on a 1M-item dataset.
-"""
-
 import numpy as np
 from typing import Union, Callable
 from filterbox.utils import get_attribute, make_empty_array
-
 
 
 def get_vals(objs: np.ndarray, obj_id_arr: np.ndarray, attr: Union[Callable, str]):
