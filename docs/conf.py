c875a71e0b8c430e1c84a5a547f8f82c854fe8e2
Theo Walker <76219068+manimino@users.noreply.github.com>
2022-09-06 14:42:38 -0400

    add duck favicon (#62)

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_favicon = 'favicon.ico'
 
 def skip(app, what, name, obj, would_skip, options):
     if name in ["__getitem__", '__init__']:
